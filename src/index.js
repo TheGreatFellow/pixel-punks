5c8f509354678ca923e726dd782cd8c4b72d2f5a
TheGreatFellow <shreyas42000@gmail.com>
2022-05-15 06:11:30 +0530

    Made it pretty✨

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import Home from './Home'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Home />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
